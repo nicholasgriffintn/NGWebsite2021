1c69fe3fa2b77e700624c395802ea11244b9c370
Nicholas Griffin <me@nicholasgriffin.co.uk>
2021-08-29 16:22:54 +0100

    fix: Adding key to blog posts

--- src/widgets/BlogPosts.js
+++ src/widgets/BlogPosts.js
@@ -3,8 +3,14 @@ import dayjs from 'dayjs';
 import Image from 'next/image';
 
 import ReturnImageFormattingUrl from '../utils/returnImageFormattingUrl';
+import React from 'react';
 
-const BlogPostsWidget = ({ posts, postsAllowLoadMore, fetchPosts, loading }) => {
+const BlogPostsWidget = ({
+  posts,
+  postsAllowLoadMore,
+  fetchPosts,
+  loading,
+}) => {
   return (
     <div className="blog-posts-widget">
       {loading === true ? (
@@ -13,52 +19,52 @@ const BlogPostsWidget = ({ posts, postsAllowLoadMore, fetchPosts, loading }) => 
         <div className="item-cards">
           {posts && posts.length > 0
             ? posts.map((post, index) => {
-              return (
-                <>
-                  {post && post.id ? (
-                    <Link key={`hp_post_${index}`} href={`/blog/${post.id}`}>
-                      <a className="item-card">
-                        {post.thumbnail ? (
-                          <div className="item-image">
-                            <Image
-                              alt={post.title}
-                              src={ReturnImageFormattingUrl(post.thumbnail)}
-                              layout="fill"
-                              objectFit="cover"
-                              quality={80}
-                              placeholder="blur"
-                              blurDataURL={`/_next/image?url=${ReturnImageFormattingUrl(
-                                post.thumbnail
-                              )}&w=16&q=1`}
-                            />
+                return (
+                  <React.Fragment key={`hp_post_${index}`}>
+                    {post && post.id ? (
+                      <Link href={`/blog/${post.id}`}>
+                        <a className="item-card">
+                          {post.thumbnail ? (
+                            <div className="item-image">
+                              <Image
+                                alt={post.title}
+                                src={ReturnImageFormattingUrl(post.thumbnail)}
+                                layout="fill"
+                                objectFit="cover"
+                                quality={80}
+                                placeholder="blur"
+                                blurDataURL={`/_next/image?url=${ReturnImageFormattingUrl(
+                                  post.thumbnail
+                                )}&w=16&q=1`}
+                              />
+                            </div>
+                          ) : null}
+                          <div className="item-content">
+                            <h3>{post.title}</h3>
+                            <p>{post.description}</p>
+                            {post.createdAt ? (
+                              <span className="item-card__meta">
+                                Posted:{' '}
+                                {dayjs(post.createdAt).format(
+                                  'dddd, MMMM D YYYY h:mm a'
+                                )}
+                              </span>
+                            ) : null}
+                            {post.createdAt !== post.updatedAt ? (
+                              <span className="item-card__meta">
+                                Updated:{' '}
+                                {dayjs(post.updatedAt).format(
+                                  'dddd, MMMM D YYYY h:mm a'
+                                )}
+                              </span>
+                            ) : null}
                           </div>
-                        ) : null}
-                        <div className="item-content">
-                          <h3>{post.title}</h3>
-                          <p>{post.description}</p>
-                          {post.createdAt ? (
-                            <span className="item-card__meta">
-                              Posted:{' '}
-                              {dayjs(post.createdAt).format(
-                                'dddd, MMMM D YYYY h:mm a'
-                              )}
-                            </span>
-                          ) : null}
-                          {post.createdAt !== post.updatedAt ? (
-                            <span className="item-card__meta">
-                              Updated:{' '}
-                              {dayjs(post.updatedAt).format(
-                                'dddd, MMMM D YYYY h:mm a'
-                              )}
-                            </span>
-                          ) : null}
-                        </div>
-                      </a>
-                    </Link>
-                  ) : null}
-                </>
-              );
-            })
+                        </a>
+                      </Link>
+                    ) : null}
+                  </React.Fragment>
+                );
+              })
             : null}
         </div>
       )}
