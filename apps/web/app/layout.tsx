d2fdc648f8a8eb1bd9ac1369d7ba01ae6ee729ee
Nicholas Griffin <nicholas.griffin@bbc.co.uk>
2024-07-21 18:34:15 +0100

    chore: configuring biome

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,69 +1,72 @@
 import type { Metadata } from "next";
-import { Open_Sans as FontSans } from "next/font/google"
+import { Open_Sans as FontSans } from "next/font/google";
 
 import "./globals.css";
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
 const fontSans = FontSans({
-  subsets: ["latin"],
-  variable: "--font-sans",
-})
+	subsets: ["latin"],
+	variable: "--font-sans",
+});
 
 export const metadata: Metadata = {
-  title: "Nicholas Griffin",
-  description: "Software Developer, Blogger and Technology Enthusiast",
-  applicationName: "Nicholas Griffin",
-  authors: [
-    {
-      url: "https://nicholasgriffin.dev",
-      name: "Nicholas Griffin",
-    }
-  ],
-  keywords: [
-    "Nicholas Griffin",
-    "Software Developer",
-  ],
-  robots: "index, follow",
-  manifest: "/manifest.json",
-  metadataBase: new URL('https://nicholasgriffin.dev'),
-  openGraph: {
-    title: "Nicholas Griffin",
-    description: "Software Developer, Blogger and Technology Enthusiast",
-    url: "https://nicholasgriffin.dev",
-    type: "website",
-    locale: "en_US",
-    siteName: "Nicholas Griffin",
-    images: [
-      {
-        url: "/images/social.jpeg",
-        width: 1200,
-        height: 630,
-        alt: "Nicholas Griffin",
-      },
-    ],
-  },
-  twitter: {
-    card: "summary_large_image", creator: "@ngriffin_uk", "images": "/images/social.jpeg",
-  },
+	title: "Nicholas Griffin",
+	description: "Software Developer, Blogger and Technology Enthusiast",
+	applicationName: "Nicholas Griffin",
+	authors: [
+		{
+			url: "https://nicholasgriffin.dev",
+			name: "Nicholas Griffin",
+		},
+	],
+	keywords: ["Nicholas Griffin", "Software Developer"],
+	robots: "index, follow",
+	manifest: "/manifest.json",
+	metadataBase: new URL("https://nicholasgriffin.dev"),
+	openGraph: {
+		title: "Nicholas Griffin",
+		description: "Software Developer, Blogger and Technology Enthusiast",
+		url: "https://nicholasgriffin.dev",
+		type: "website",
+		locale: "en_US",
+		siteName: "Nicholas Griffin",
+		images: [
+			{
+				url: "/images/social.jpeg",
+				width: 1200,
+				height: 630,
+				alt: "Nicholas Griffin",
+			},
+		],
+	},
+	twitter: {
+		card: "summary_large_image",
+		creator: "@ngriffin_uk",
+		images: "/images/social.jpeg",
+	},
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en" className="dark" style={{
-      colorScheme: "dark"
-    }}>
-      <body
-        className={cn(
-          "min-h-screen bg-background font-sans antialiased",
-          fontSans.variable
-        )}
-      >
-        {children}
-      </body>
-    </html>
-  );
+	return (
+		<html
+			lang="en"
+			className="dark"
+			style={{
+				colorScheme: "dark",
+			}}
+		>
+			<body
+				className={cn(
+					"min-h-screen bg-background font-sans antialiased",
+					fontSans.variable,
+				)}
+			>
+				{children}
+			</body>
+		</html>
+	);
 }
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,16 +1,21 @@
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 export default function Home() {
-  return (
-    <main>
-       <div className="pt-6 pb-6 flex flex-col items-center space-y-4 text-center">
-        <Avatar className="w-16 h-16">
-          <AvatarImage src="/avatar.png" />
-          <AvatarFallback>Avatar</AvatarFallback>
-        </Avatar>
-        <h1 className="text-3xl font-bold text-white-800">Hi! I'm Nicholas Griffin!</h1>
-        <p className="text-white-600">I'm currently rebuilding my website after a bit of a break, please bare with.</p>
-      </div>
-    </main>
-  );
+	return (
+		<main>
+			<div className="pt-6 pb-6 flex flex-col items-center space-y-4 text-center">
+				<Avatar className="w-16 h-16">
+					<AvatarImage src="/avatar.png" />
+					<AvatarFallback>Avatar</AvatarFallback>
+				</Avatar>
+				<h1 className="text-3xl font-bold text-white-800">
+					Hi! I'm Nicholas Griffin!
+				</h1>
+				<p className="text-white-600">
+					I'm currently rebuilding my website after a bit of a break, please
+					bare with.
+				</p>
+			</div>
+		</main>
+	);
 }
