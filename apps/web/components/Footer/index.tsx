8f2b760aa329fa53a99805043374ffdd7248f89f
Nicholas <nicholas.griffin@bbc.co.uk>
2024-10-26 22:23:30 +0100

    chore: changing site content to CC license

--- apps/web/components/Footer/index.tsx
+++ apps/web/components/Footer/index.tsx
@@ -3,29 +3,38 @@ import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
 
 export function Footer() {
 	return (
-		<footer className="w-full">
-			<div className="container flex items-center items-center justify-center py-4 gap-4">
-				<div className="flex items-center space-x-4 py-2">
-					<Avatar>
-						<AvatarImage src="/avatar.png" />
-						<AvatarFallback>NG</AvatarFallback>
-					</Avatar>
-				</div>
-				<div>
-					<span>Copyright © {new Date().getFullYear()} Nicholas Griffin</span>
-				</div>
-				<div>
-					<nav className="flex items-center space-x-6">
-						<Link
-							href="https://github.com/nicholasgriffintn/NGWebsite2021"
-							target="_blank"
-							rel="noopener noreferrer"
-						>
-							Source Code
-						</Link>
-					</nav>
-				</div>
-			</div>
-		</footer>
-	);
+    <footer className="w-full">
+      <div className="container flex items-center items-center justify-center py-4 gap-4">
+        <div className="flex items-center space-x-4 py-2">
+          <Avatar>
+            <AvatarImage src="/avatar.png" />
+            <AvatarFallback>NG</AvatarFallback>
+          </Avatar>
+        </div>
+        <div>
+          <span>
+            © {new Date().getFullYear()} Nicholas Griffin.{' '}
+            <Link
+              href="https://creativecommons.org/licenses/by-sa/4.0/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              CC BY-SA 4.0
+            </Link>
+          </span>
+        </div>
+        <div>
+          <nav className="flex items-center space-x-6">
+            <Link
+              href="https://github.com/nicholasgriffintn/NGWebsite2021"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Source Code
+            </Link>
+          </nav>
+        </div>
+      </div>
+    </footer>
+  );
 }
