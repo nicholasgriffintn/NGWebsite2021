ad53a8700d6c36f8c898fa3adc37d23fb97177e1
Nicholas <nicholas.griffin@bbc.co.uk>
2024-10-27 16:49:02 +0000

    chore: snippets styles

--- apps/web/components/SnippetsList/index.tsx
+++ apps/web/components/SnippetsList/index.tsx
@@ -1,13 +1,6 @@
 import type { GitHubGists } from '@/types/github';
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Link } from '@/components/Link';
-import { buttonVariants } from '@/components/ui/button';
 
 export function SnippetsList({ gists }: { gists: GitHubGists | undefined }) {
   return (
@@ -19,117 +12,114 @@ export function SnippetsList({ gists }: { gists: GitHubGists | undefined }) {
               <Card key={gist.id} className="overflow-hidden">
                 <CardHeader>
                   {gist.description && (
-                    <CardTitle>{gist.description}</CardTitle>
-                  )}
-                </CardHeader>
-                <CardContent>
-                  {gist.files && Object.keys(gist.files).length > 0 ? (
-                    <ul className="pb-5">
-                      <li className="item-content-files-title">
-                        <span>Files:</span>
-                      </li>
-                      {Object.keys(gist.files).map((file, index) => {
-                        if (!gist.files[file]) return null;
-
-                        return (
-                          <li
-                            key={`file-${gist.files[file].filename}-${index}`}
-                            className="item-content-files-file"
-                          >
-                            <strong>{gist.files[file].filename}</strong>
-                            <span className="item-card__meta">
-                              Type: {gist.files[file].type}
-                            </span>
-                            <span className="item-card__meta">
-                              Size: {gist.files[file].size}
-                            </span>
-                            {gist.files[file].language ? (
-                              <span className="item-card__meta">
-                                <span
-                                  className="item-card__language-icon"
-                                  style={{
-                                    color:
-                                      gist.files[file].language === 'JavaScript'
-                                        ? '#f7df1c'
-                                        : gist.files[file].language === 'PHP'
-                                        ? '#777bb4'
-                                        : gist.files[file].language === 'HTML'
-                                        ? '#e34f25'
-                                        : gist.files[file].language === 'Vue'
-                                        ? '#42b883'
-                                        : gist.files[file].language === 'Go'
-                                        ? '#00ADD8'
-                                        : gist.files[file].language === 'C++'
-                                        ? '#f34b7d'
-                                        : gist.files[file].language === 'C#'
-                                        ? '#178600'
-                                        : gist.files[file].language === 'Python'
-                                        ? '#3572A5'
-                                        : gist.files[file].language ===
-                                          'TypeScript'
-                                        ? '#2b7489'
-                                        : gist.files[file].language === 'CSS'
-                                        ? '#563d7c'
-                                        : gist.files[file].language === 'Swift'
-                                        ? '#F05138'
-                                        : gist.files[file].language === 'Java'
-                                        ? '#b07219'
-                                        : gist.files[file].language === 'C'
-                                        ? '#555555'
-                                        : gist.files[file].language === 'Ruby'
-                                        ? '#701516'
-                                        : gist.files[file].language ===
-                                          'CoffeeScript'
-                                        ? '#244776'
-                                        : gist.files[file].language === 'Rust'
-                                        ? '#dea584'
-                                        : gist.files[file].language === 'Dart'
-                                        ? '#89e051'
-                                        : gist.files[file].language ===
-                                          'PowerShell'
-                                        ? '#012456'
-                                        : gist.files[file].language ===
-                                          'Dockerfile'
-                                        ? '#384d54'
-                                        : gist.files[file].language === 'YAML'
-                                        ? '#000'
-                                        : '#fff',
-                                  }}
-                                >
-                                  ●
-                                </span>{' '}
-                                {gist.files[file].language}
-                              </span>
-                            ) : null}
-                            <span className="item-card__meta">
-                              <a
-                                href={gist.files[file].raw_url}
-                                target="_blank"
-                                rel="noopener noreferrer"
-                              >
-                                View File
-                              </a>
-                            </span>
-                          </li>
-                        );
-                      })}
-                    </ul>
-                  ) : null}
-                  <div className="w-full flex justify-left">
                     <Link
                       href={gist.html_url}
                       muted
                       target="_blank"
                       rel="noopener noreferer"
-                      className={buttonVariants({
-                        variant: 'outline',
-                        size: 'sm',
-                      })}
                       underline={false}
                     >
-                      View Gist
+                      <CardTitle>{gist.description}</CardTitle>
                     </Link>
-                  </div>
+                  )}
+                </CardHeader>
+                <CardContent>
+                  {gist.files && Object.keys(gist.files).length > 0 ? (
+                    <div className="pb-5">
+                      <span>Files:</span>
+                      <ul className="pt-2">
+                        {Object.keys(gist.files).map((file, index) => {
+                          if (!gist?.files?.[file]) return null;
+
+                          const fileData = gist.files[file];
+
+                          const colour =
+                            fileData.language === 'JavaScript'
+                              ? '#f7df1c'
+                              : fileData.language === 'PHP'
+                              ? '#777bb4'
+                              : fileData.language === 'HTML'
+                              ? '#e34f25'
+                              : fileData.language === 'Vue'
+                              ? '#42b883'
+                              : fileData.language === 'Go'
+                              ? '#00ADD8'
+                              : fileData.language === 'C++'
+                              ? '#f34b7d'
+                              : fileData.language === 'C#'
+                              ? '#178600'
+                              : fileData.language === 'Python'
+                              ? '#3572A5'
+                              : fileData.language === 'TypeScript'
+                              ? '#2b7489'
+                              : fileData.language === 'CSS'
+                              ? '#563d7c'
+                              : fileData.language === 'Swift'
+                              ? '#F05138'
+                              : fileData.language === 'Java'
+                              ? '#b07219'
+                              : fileData.language === 'C'
+                              ? '#555555'
+                              : fileData.language === 'Ruby'
+                              ? '#701516'
+                              : fileData.language === 'CoffeeScript'
+                              ? '#244776'
+                              : fileData.language === 'Rust'
+                              ? '#dea584'
+                              : fileData.language === 'Dart'
+                              ? '#89e051'
+                              : fileData.language === 'PowerShell'
+                              ? '#012456'
+                              : fileData.language === 'Dockerfile'
+                              ? '#384d54'
+                              : fileData.language === 'YAML'
+                              ? '#333'
+                              : '#fff';
+
+                          return (
+                            <li
+                              key={`file-${fileData.filename}-${index}`}
+                              className="mb-4 grid grid-cols-[25px_1fr] items-start pb-4 last:mb-0 last:pb-0"
+                            >
+                              <span
+                                className="flex h-2 w-2 translate-y-1 rounded-full"
+                                style={{
+                                  backgroundColor: colour,
+                                }}
+                              />
+                              <div className="space-y-1">
+                                <p className="text-sm font-medium leading-none">
+                                  {fileData.filename}
+                                </p>
+                                <p className="text-sm text-muted-foreground">
+                                  Type: {fileData.type}
+                                </p>
+                                <p className="text-sm text-muted-foreground">
+                                  Size: {fileData.size}
+                                </p>
+                                {fileData.language ? (
+                                  <p className="text-sm text-muted-foreground">
+                                    Language: {fileData.language}
+                                  </p>
+                                ) : null}
+                                <div className="w-full flex justify-left">
+                                  <Link
+                                    href={fileData.raw_url}
+                                    muted
+                                    target="_blank"
+                                    rel="noopener noreferer"
+                                    className="text-sm text-muted-foreground"
+                                  >
+                                    View FIle
+                                  </Link>
+                                </div>
+                              </div>
+                            </li>
+                          );
+                        })}
+                      </ul>
+                    </div>
+                  ) : null}
                 </CardContent>
               </Card>
             );
