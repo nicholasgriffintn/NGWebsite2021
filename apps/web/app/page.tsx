68984f23b4a674ff98f71c0c9766c4a569967c90
Nicholas <nicholas.griffin@bbc.co.uk>
2024-10-27 22:11:50 +0000

    chore: adjusting padding

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -47,7 +47,7 @@ export default async function Home() {
         <div className="grid grid-cols-5 gap-4">
           <div className="col-span-5 md:col-span-3 lg:col-span-4 pt-5">
             <div className="text-primary-foreground lg:max-w-[75%]">
-              <h1 className="text-2xl md:text-4xl font-bold text-primary-foreground lg:pt-5">
+              <h1 className="text-2xl md:text-4xl font-bold text-primary-foreground">
                 👋 Welcome to my website!
               </h1>
               <p>
--- apps/web/components/InnerPage/index.tsx
+++ apps/web/components/InnerPage/index.tsx
@@ -1,9 +1,9 @@
 export function InnerPage({ children }: { children: React.ReactNode }) {
   return (
     <div className="container pt-[59px] text-left">
-      <div className="pt-5 md:pt-20"></div>
+      <div className="pt-5 md:pt-10"></div>
       {children}
-      <div className="pt-5 md:pt-20"></div>
+      <div className="pt-5 md:pb-10"></div>
     </div>
   );
 }
