db288aea42e55c11c28a860b511c41c8976244cb
Nicholas <nicholas.griffin@bbc.co.uk>
2024-11-10 22:21:56 +0000

    feat: adding auth

--- apps/web/app/chat/page.tsx
+++ apps/web/app/chat/page.tsx
@@ -1,3 +1,6 @@
+import { cookies } from 'next/headers';
+import { notFound } from 'next/navigation';
+
 import { PageLayout } from '@/components/PageLayout';
 import { ChatInterface } from '@/components/ChatInterface';
 import { InnerPage } from '@/components/InnerPage';
@@ -7,7 +10,30 @@ export const metadata = {
   description: 'Start a chat with my assistant.',
 };
 
-export default function Chat() {
+async function checkAuth() {
+  const systemAuthToken = process.env.AUTH_TOKEN || '';
+
+  if (!systemAuthToken) {
+    return notFound();
+  }
+
+  const cookieStore = await cookies();
+  const userAuthToken = cookieStore.get('authToken');
+
+  if (!userAuthToken?.value) {
+    return notFound();
+  }
+
+  if (userAuthToken.value !== systemAuthToken) {
+    return notFound();
+  }
+
+  return userAuthToken.value;
+}
+
+export default async function Chat() {
+  const auth = await checkAuth();
+
   return (
     <PageLayout>
       <InnerPage isFullPage>
