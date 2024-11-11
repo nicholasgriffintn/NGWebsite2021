b714bab38289244ad30ca6f3c5e0f0ab6e952690
Nicholas <nicholas.griffin@bbc.co.uk>
2024-11-11 04:24:56 +0000

    chore: adding local calling

--- apps/web/lib/data/chat.ts
+++ apps/web/lib/data/chat.ts
@@ -1,4 +1,5 @@
 import type { ChatList } from '@/types/chat';
+import { baseUrl } from '../../app/sitemap';
 
 export async function getChat({
   id,
@@ -12,9 +13,11 @@ export async function getChat({
     return;
   }
 
-  const url = `https://assistant.nicholasgriffin.workers.dev/chat${
-    id ? `/${id}` : ''
-  }`;
+  const baseUrl =
+    process.env.NODE_ENV === 'development'
+      ? 'http://localhost:8787'
+      : 'https://assistant.nicholasgriffin.workers.dev';
+  const url = `${baseUrl}/chat${id ? `/${id}` : ''}`;
 
   const res = await fetch(url, {
     headers: {
@@ -69,26 +72,24 @@ export async function createChat({
     return '';
   }
 
-  console.log('Creating chat', chatId, message);
-
-  const res = await fetch(
-    `https://assistant.nicholasgriffin.workers.dev/chat`,
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'User-Agent': 'NGWeb',
-        Authorization: `Bearer ${token}`,
-      },
-      body: JSON.stringify({
-        chat_id: chatId,
-        input: message,
-        date: new Date().toISOString(),
-        model: model || 'hermes-2-pro-mistral-7b',
-        location: {},
-      }),
-    }
-  );
+  const baseUrl =
+    process.env.NODE_ENV === 'development'
+      ? 'http://localhost:8787'
+      : 'https://assistant.nicholasgriffin.workers.dev';
+  const res = await fetch(`${baseUrl}/chat`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'User-Agent': 'NGWeb',
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({
+      chat_id: chatId,
+      input: message,
+      date: new Date().toISOString(),
+      model: model || 'hermes-2-pro-mistral-7b',
+    }),
+  });
 
   if (!res.ok) {
     console.error('Error fetching data from AI', res.statusText);
