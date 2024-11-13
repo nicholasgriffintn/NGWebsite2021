9aac24367182245ae92714b1e25d22531ea8e1d9
Nicholas <nicholas.griffin@bbc.co.uk>
2024-11-13 22:06:08 +0000

    feat: adding more compatible models

--- apps/web/lib/ai/models.ts
+++ apps/web/lib/ai/models.ts
@@ -3,6 +3,7 @@ export const defaultModel = 'hermes-2-pro-mistral-7b';
 export const modelsOptions = [
   { id: 'hermes-2-pro-mistral-7b', name: 'Hermes 2 Pro - Mistral 7B' },
   { id: 'llama-3.1-70b-instruct', name: 'Llama 3.1 - 70B Instruct' },
+  { id: 'llama-3.2-1b-instruct', name: 'Llama 3.2 - 1B Instruct' },
   { id: 'llama-3.2-3b-instruct', name: 'Llama 3.2 - 3B Instruct' },
   { id: 'claude-3-5-sonnet', name: 'Claude 3.5 - Sonnet' },
   { id: 'claude-3.5-haiku', name: 'Claude 3.5 - Haiku' },
@@ -21,6 +22,22 @@ export const modelsOptions = [
   {
     id: 'llama-3.1-sonar-huge-128k-online',
     name: 'Llama 3.1 - Sonar Huge 128k Online',
+  },
+  { id: 'openchat', name: 'OpenChat' },
+  { id: 'phi-2', name: 'Phi 2' },
+  { id: 'sqlcoder', name: 'SQLCoder' },
+  { id: 'tinyllama', name: 'TinyLlama' },
+  { id: 'una-cybertron-7b-v2', name: 'Una Cybertron 7B V2' },
+  { id: 'deepseek-coder-6.7b', name: 'DeepSeek Coder 6.7B' },
+  { id: 'stable-diffusion-1.5-img2img', name: 'Stable Diffusion 1.5 Img2Img' },
+  {
+    id: 'stable-diffusion-1.5-inpainting',
+    name: 'Stable Diffusion 1.5 Inpainting',
+  },
+  { id: 'stable-diffusion-cl-base-1.0', name: 'Stable Diffusion CL Base 1.0' },
+  {
+    id: 'stable-diffusion-xl-lightning',
+    name: 'Stable Diffusion XL Lightning',
   },
   { id: 'flux', name: 'Flux' },
 ];
