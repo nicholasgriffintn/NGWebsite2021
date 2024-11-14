76b1e8574085090b67d74d0bf9b756e0e0792e05
Nicholas <nicholas.griffin@bbc.co.uk>
2024-11-14 00:05:26 +0000

    chore: handle errors better

--- apps/web/lib/data/chat.ts
+++ apps/web/lib/data/chat.ts
@@ -5,48 +5,52 @@ export async function getChatKeys({
 }: {
   token: string;
 }): Promise<ChatList | undefined> {
-  if (!token) {
-    console.error('No token provided');
-    return;
+  try {
+    if (!token) {
+      console.error('No token provided');
+      return;
+    }
+
+    const baseUrl =
+      process.env.NODE_ENV === 'development'
+        ? 'http://localhost:8787'
+        : 'https://assistant.nicholasgriffin.workers.dev';
+    const url = `${baseUrl}/chat`;
+
+    const res = await fetch(url, {
+      headers: {
+        'Content-Type': 'application/json',
+        'User-Agent': 'NGWeb',
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    if (!res.ok) {
+      console.error('Error fetching data from AI', res.statusText);
+      return;
+    }
+
+    const data: any = await res.json();
+
+    const keys = data?.response?.keys;
+
+    if (!keys) {
+      console.error('No keys found in response');
+      return;
+    }
+
+    const chatList = keys?.map((chat) => {
+      return {
+        ...chat,
+        id: chat.id || chat.name,
+        title: chat.title || chat.name || '',
+      };
+    });
+
+    return chatList;
+  } catch (error) {
+    console.error('Error getting chat keys', error);
   }
-
-  const baseUrl =
-    process.env.NODE_ENV === 'development'
-      ? 'http://localhost:8787'
-      : 'https://assistant.nicholasgriffin.workers.dev';
-  const url = `${baseUrl}/chat`;
-
-  const res = await fetch(url, {
-    headers: {
-      'Content-Type': 'application/json',
-      'User-Agent': 'NGWeb',
-      Authorization: `Bearer ${token}`,
-    },
-  });
-
-  if (!res.ok) {
-    console.error('Error fetching data from AI', res.statusText);
-    return;
-  }
-
-  const data: any = await res.json();
-
-  const keys = data?.response?.keys;
-
-  if (!keys) {
-    console.error('No keys found in response');
-    return;
-  }
-
-  const chatList = keys?.map((chat) => {
-    return {
-      ...chat,
-      id: chat.id || chat.name,
-      title: chat.title || chat.name || '',
-    };
-  });
-
-  return chatList;
 }
 
 export async function getChat({
@@ -56,33 +60,37 @@ export async function getChat({
   id: string;
   token: string;
 }): Promise<ChatMessage[]> {
-  if (!token || !id) {
-    console.error('No token or id provided');
-    return [];
+  try {
+    if (!token || !id) {
+      console.error('No token or id provided');
+      return [];
+    }
+
+    const baseUrl =
+      process.env.NODE_ENV === 'development'
+        ? 'http://localhost:8787'
+        : 'https://assistant.nicholasgriffin.workers.dev';
+    const url = `${baseUrl}/chat/${id}`;
+
+    const res = await fetch(url, {
+      headers: {
+        'Content-Type': 'application/json',
+        'User-Agent': 'NGWeb',
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    if (!res.ok) {
+      console.error('Error fetching data from AI', res.statusText);
+      return [];
+    }
+
+    const data: ChatMessage[] = await res.json();
+
+    return data;
+  } catch (error) {
+    console.error('Error getting chat', error);
   }
-
-  const baseUrl =
-    process.env.NODE_ENV === 'development'
-      ? 'http://localhost:8787'
-      : 'https://assistant.nicholasgriffin.workers.dev';
-  const url = `${baseUrl}/chat/${id}`;
-
-  const res = await fetch(url, {
-    headers: {
-      'Content-Type': 'application/json',
-      'User-Agent': 'NGWeb',
-      Authorization: `Bearer ${token}`,
-    },
-  });
-
-  if (!res.ok) {
-    console.error('Error fetching data from AI', res.statusText);
-    return [];
-  }
-
-  const data: ChatMessage[] = await res.json();
-
-  return data;
 }
 
 export async function createChat({
@@ -96,44 +104,48 @@ export async function createChat({
   message: string;
   model?: string;
 }): Promise<ChatMessage[]> {
-  if (!token || !chatId || !message) {
-    console.error('No token provided');
-    return [];
+  try {
+    if (!token || !chatId || !message) {
+      console.error('No token provided');
+      return [];
+    }
+
+    const baseUrl =
+      process.env.NODE_ENV === 'development'
+        ? 'http://localhost:8787'
+        : 'https://assistant.nicholasgriffin.workers.dev';
+
+    const res = await fetch(`${baseUrl}/chat`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'User-Agent': 'NGWeb',
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({
+        chat_id: chatId,
+        input:
+          model === 'flux'
+            ? {
+                prompt: message,
+              }
+            : message,
+        date: new Date().toISOString(),
+        model: model || 'hermes-2-pro-mistral-7b',
+      }),
+    });
+
+    if (!res.ok) {
+      console.error('Error fetching data from AI', res.statusText);
+      return [];
+    }
+
+    const data: ChatMessage[] = await res.json();
+
+    return data;
+  } catch (error) {
+    console.error('Error creating chat', error);
   }
-
-  const baseUrl =
-    process.env.NODE_ENV === 'development'
-      ? 'http://localhost:8787'
-      : 'https://assistant.nicholasgriffin.workers.dev';
-
-  const res = await fetch(`${baseUrl}/chat`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'User-Agent': 'NGWeb',
-      Authorization: `Bearer ${token}`,
-    },
-    body: JSON.stringify({
-      chat_id: chatId,
-      input:
-        model === 'flux'
-          ? {
-              prompt: message,
-            }
-          : message,
-      date: new Date().toISOString(),
-      model: model || 'hermes-2-pro-mistral-7b',
-    }),
-  });
-
-  if (!res.ok) {
-    console.error('Error fetching data from AI', res.statusText);
-    return [];
-  }
-
-  const data: ChatMessage[] = await res.json();
-
-  return data;
 }
 
 export async function sendTranscription({
@@ -143,39 +155,44 @@ export async function sendTranscription({
   token: string;
   audio: Blob;
 }): Promise<string> {
-  if (!token || !audio) {
-    throw new Error('No token provided');
+  try {
+    if (!token || !audio) {
+      throw new Error('No token provided');
+    }
+
+    const baseUrl =
+      process.env.NODE_ENV === 'development'
+        ? 'http://localhost:8787'
+        : 'https://assistant.nicholasgriffin.workers.dev';
+
+    const formData = new FormData();
+    formData.append('audio', audio);
+
+    const res = await fetch(`${baseUrl}/chat/transcribe`, {
+      method: 'POST',
+      headers: {
+        'User-Agent': 'NGWeb',
+        Authorization: `Bearer ${token}`,
+      },
+      body: formData,
+    });
+
+    if (!res.ok) {
+      console.error('Error fetching data from AI', res.statusText);
+      throw new Error('Error fetching data from AI');
+    }
+
+    const data: {
+      response: {
+        content: string;
+      };
+    } = await res.json();
+
+    return data.response.content;
+  } catch (error) {
+    console.error('Error transcribing audio', error);
+    throw new Error('Error transcribing audio');
   }
-
-  const baseUrl =
-    process.env.NODE_ENV === 'development'
-      ? 'http://localhost:8787'
-      : 'https://assistant.nicholasgriffin.workers.dev';
-
-  const formData = new FormData();
-  formData.append('audio', audio);
-
-  const res = await fetch(`${baseUrl}/chat/transcribe`, {
-    method: 'POST',
-    headers: {
-      'User-Agent': 'NGWeb',
-      Authorization: `Bearer ${token}`,
-    },
-    body: formData,
-  });
-
-  if (!res.ok) {
-    console.error('Error fetching data from AI', res.statusText);
-    throw new Error('Error fetching data from AI');
-  }
-
-  const data: {
-    response: {
-      content: string;
-    };
-  } = await res.json();
-
-  return data.response.content;
 }
 
 export async function sendFeedback({
@@ -187,32 +204,36 @@ export async function sendFeedback({
   logId: string;
   feedback: string;
 }): Promise<void> {
-  if (!token || !logId || !feedback) {
-    console.error('No token provided');
-    return;
+  try {
+    if (!token || !logId || !feedback) {
+      console.error('No token provided');
+      return;
+    }
+
+    const baseUrl =
+      process.env.NODE_ENV === 'development'
+        ? 'http://localhost:8787'
+        : 'https://assistant.nicholasgriffin.workers.dev';
+    const res = await fetch(`${baseUrl}/chat/feedback`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'User-Agent': 'NGWeb',
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({
+        logId,
+        feedback: feedback === 'positive' ? 1 : -1,
+      }),
+    });
+
+    if (!res.ok) {
+      console.error('Error fetching data from AI', res.statusText);
+      return;
+    }
+
+    const data = await res.json();
+  } catch (error) {
+    console.error('Error sending feedback', error);
   }
-
-  const baseUrl =
-    process.env.NODE_ENV === 'development'
-      ? 'http://localhost:8787'
-      : 'https://assistant.nicholasgriffin.workers.dev';
-  const res = await fetch(`${baseUrl}/chat/feedback`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'User-Agent': 'NGWeb',
-      Authorization: `Bearer ${token}`,
-    },
-    body: JSON.stringify({
-      logId,
-      feedback: feedback === 'positive' ? 1 : -1,
-    }),
-  });
-
-  if (!res.ok) {
-    console.error('Error fetching data from AI', res.statusText);
-    return;
-  }
-
-  const data = await res.json();
 }
