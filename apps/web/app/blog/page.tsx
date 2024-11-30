edf79b7a42b58d4a5b157f486c0f25b22289eeaa
Nicholas <nicholas.griffin@bbc.co.uk>
2024-11-30 18:26:55 +0000

    feat: adding openrouter bookmark

--- apps/web/app/blog/page.tsx
+++ apps/web/app/blog/page.tsx
@@ -45,7 +45,7 @@ export default async function Home({ searchParams }) {
           </div>
         </div>
         {data?.posts && (
-          <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-4">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
             {data.posts.map((post) => (
               <BlogCard key={post.slug} post={post} />
             ))}
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -100,7 +100,7 @@ export default async function Home() {
             </div>
             <section>
               {data?.blogPosts && (
-                <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-4">
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
                   {data.blogPosts.map((post) => (
                     <BlogCard key={post.slug} post={post} />
                   ))}
